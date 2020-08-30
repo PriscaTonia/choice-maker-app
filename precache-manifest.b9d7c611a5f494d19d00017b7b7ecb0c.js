f9850ff570334b95320170a7886e1bd4a184d8d0
PriscaTonia <priscaebube@gmail.com>
2020-08-30 21:06:27 +0100

    Updates

---
+++ precache-manifest.b9d7c611a5f494d19d00017b7b7ecb0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94bf69313605b553a0d707b670c902de",
+    "url": "/choice-maker-app/index.html"
+  },
+  {
+    "revision": "3895114f551918ab7ce8",
+    "url": "/choice-maker-app/static/css/main.57e0317a.chunk.css"
+  },
+  {
+    "revision": "e48d0b1d76245365cdd7",
+    "url": "/choice-maker-app/static/js/2.9e3c7291.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/choice-maker-app/static/js/2.9e3c7291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3895114f551918ab7ce8",
+    "url": "/choice-maker-app/static/js/main.7ba5ac7e.chunk.js"
+  },
+  {
+    "revision": "4fb229005eac691b9534",
+    "url": "/choice-maker-app/static/js/runtime-main.1d9c0ba8.js"
+  }
+]);
